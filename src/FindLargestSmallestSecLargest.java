be9cafef869d06772b18d8dca89f66f773123489
Edy Rodriguez <Edy Rodriguez@LAPTOP-QFB1AMLV>
2023-08-27 20:30:57 -0600

    Update with comments.

--- src/FindLargestSmallestSecLargest.java
+++ src/FindLargestSmallestSecLargest.java
@@ -10,18 +10,24 @@ public class FindLargestSmallestSecLargest {
         int smallest = arr[0];
         int secLargest = arr[0];
 
+        // Iterate through the array
         for (int i = 1; i < lengthOfArray; i++) {
             if (arr[i] > largest) {
+                // If the current element is larger than the current largest
+                // Update second largest and largest accordingly
                 secLargest = largest;
                 largest = arr[i];
             } else if (arr[i] < smallest) {
+                // If the current element is smaller than the current smallest
+                // Update second largest and smallest accordingly
                 secLargest = smallest;
                 smallest = arr[i];
             } else if (arr[i] > secLargest) {
+                // If the current element is larger than the current second largest
+                // Update second largest accordingly
                 secLargest = arr[i];
             }
-        }
-        
+		}
         // Print the results after the loop
         System.out.println("Largest: " + largest);
         System.out.println("Smallest: " + smallest);
